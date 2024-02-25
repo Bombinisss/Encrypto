bc7e66fcdb4aed1a87d792a1a2515c4d12140670
Bombinisss <antekbom@gmail.com>
2024-02-25 18:57:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Encrypto
+
+Initial commit.
+
+### Depediencies
+
+Made using [eframe](https://github.com/emilk/egui/tree/master/crates/eframe), a framework for writing apps using [egui](https://github.com/emilk/egui/).
---
+++ src/main.rs
@@ -0,0 +1,23 @@
+#![warn(clippy::all, rust_2018_idioms)]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+fn main() -> eframe::Result<()> {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+
+    let native_options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([400.0, 300.0])
+            .with_min_inner_size([300.0, 220.0])
+            .with_icon(
+                // NOE: Adding an icon is optional
+                eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-1024.png")[..])
+                    .unwrap(),
+            ),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Encrypto",
+        native_options,
+        Box::new(|cc| Box::new(encrypto::EncryptoInterface::new(cc))),
+    )
+}
